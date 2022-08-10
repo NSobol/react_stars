f083045c29dc97694afb48d563cc25f2406634a0
Soboleva Natalia <nsoboleva1979@rambler.ru>
2022-08-11 01:08:40 +0300

    Второй комит

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Stars from "./components/Stars.jsx";
 
 function App() {
-  return <Stars count={2} />;
+  return <Stars count={1} />;
 }
 
 export default App;
--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import Star from "./Star";
 
-function Stars({count}) {
-	const stars = [];
-	let sum;
-	if (count >= 1 && count <= 5 && !isNaN(count)) {
-		sum = count;
-	} else {
-		sum = 0;
-	}
-  for (let i = 1; i <= sum; i++) {
+function Stars({ count }) {
+  const stars = [];
+  let sum;
+  sum = count >= 1 && count <= 5 && !isNaN(count) ? count : 0;
+
+  for (let i = 0; i < sum; i++) {
     stars.push("1");
   }
-  console.log(stars);
+
   return (
     <ul className="card-body-stars u-clearfix">
       {stars.map((star, index) => {
