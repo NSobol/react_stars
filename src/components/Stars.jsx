7d98470791a363be8ecf0f34efe5b70e8e8a8c36
Soboleva Natalia <nsoboleva1979@rambler.ru>
2022-08-11 01:29:59 +0300

    Добавлены значения по умолчанию и проверка типа

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import propTypes from "prop-types";
 import Star from "./Star";
 
 function Stars({ count }) {
@@ -6,21 +7,23 @@ function Stars({ count }) {
   let sum;
   sum = count >= 1 && count <= 5 && !isNaN(count) ? count : 0;
 
-  for (let i = 0; i < sum; i++) {
+  for (let i = 1; i <= sum; i++) {
     stars.push("1");
   }
 
   return (
     <ul className="card-body-stars u-clearfix">
-      {stars.map((star, index) => {
-        return (
-          <li key={index}>
-            <Star />
-          </li>
-        );
-      })}
+      {stars.map((star, index) => <Star key={index} />)}
     </ul>
   );
 }
+
+Stars.defaultProps = {
+  count: 0,
+};
+
+Stars.propTypes = {
+  count: propTypes.number.isRequired,
+};
 
 export default Stars;
